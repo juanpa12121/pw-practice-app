ba74f2fb69713a8b8ae4ddd3779ce1eab38521a7
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2025-06-03 17:34:16 -0500

    24. Locator Syntax Rules

---
+++ tests/firstTest1.spec.ts
@@ -0,0 +1,37 @@
+import { test } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:4200/");
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
+});
+
+test('Locator syntax rules', async ({ page }) => {
+    //by Tag name
+    await page.locator('input').first().click();
+
+    //by ID
+    await page.locator('#inputEmail1').click();
+
+    //by Class value
+    page.locator('.shape-rectangle');
+
+    //by attribute
+    page.locator('[placeholder="Email"]');
+
+    //by Class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different selectors
+    page.locator('input[placeholder="Email"][nbinput]')
+
+    //by XPath (NOT RECOMMENDED)
+    page.locator('//*[@id="inputEmail1"]')
+
+    //by partial text match
+    page.locator(':text("Using")')
+
+    //by exact text match
+    page.locator(':text-is("Using the Grid")')
+
+});

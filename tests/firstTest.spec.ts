8a824bc08f7f67a29595c9e43cb639ec58765cd6
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2025-06-03 15:06:20 -0500

    22. Hooks and Flow Control

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,8 +1,54 @@
-import { test } from '@playwright/test';
+import { test } from "@playwright/test";
 
-test('The first test', async ({page}) => {
-    await page.goto('http://localhost:4200/');
-    await page.getByText("Forms").click();
-    await page.getByText("Form Layouts").click();
-})
+test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:4200/");
+});
 
+test.describe("Suite1", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'Light' }).click();
+    });
+
+    test("Click Dark", async ({ page }) => {
+        await page.getByText("Dark").click();
+    });
+
+    test("Click Cosmic", async ({ page }) => {
+        await page.getByText("Cosmic").click();
+    });
+});
+
+test.describe("Suite2", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText("Auth").click();
+    });
+
+    test("Login", async ({ page }) => {
+        await page.getByText("Login").click();
+        await page.getByPlaceholder("Email address").fill("admin");
+        await page.getByPlaceholder("Password").fill("admin");
+
+    });
+
+    test("Register", async ({ page }) => {
+        await page.getByText("Register").click();
+        await page.getByPlaceholder("Full name").fill("Pedro Perez");
+        await page.getByPlaceholder("Email address").fill("admin@example.com");
+        await page.locator("#input-password").fill("admin");
+        await page.getByPlaceholder("Confirm password").fill("admin");
+    });
+});
+
+test.describe("Suite3", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText("Forms").click();
+    });
+
+    test("The first test1", async ({ page }) => {
+        await page.getByText("Form Layouts").click();
+    });
+
+    test("Navigate to datepicker page1", async ({ page }) => {
+        await page.getByText("Datepicker").click();
+    });
+});

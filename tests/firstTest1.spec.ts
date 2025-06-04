e547eb8870b44fbc43ccb7d3ea07b82d12ec391d
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2025-06-03 20:59:56 -0500

    Update firstTest1.spec.ts

--- tests/firstTest1.spec.ts
+++ tests/firstTest1.spec.ts
@@ -1,49 +1,49 @@
 import { test } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:4200/");
-    await page.getByText("Forms").click();
-    await page.getByText("Form Layouts").click();
+  await page.goto("http://localhost:4200/");
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
 });
 
-test('Locator syntax rules', async ({ page }) => {
-    //by Tag name
-    await page.locator('input').first().click();
+test("Locator syntax rules", async ({ page }) => {
+  //by Tag name
+  await page.locator("input").first().click();
 
-    //by ID
-    await page.locator('#inputEmail1').click();
+  //by ID
+  await page.locator("#inputEmail1").click();
 
-    //by Class value
-    page.locator('.shape-rectangle');
+  //by Class value
+  page.locator(".shape-rectangle");
 
-    //by attribute
-    page.locator('[placeholder="Email"]');
+  //by attribute
+  page.locator('[placeholder="Email"]');
 
-    //by Class value (full)
-    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+  //by Class value (full)
+  page.locator(
+    '[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]'
+  );
 
-    //combine different selectors
-    page.locator('input[placeholder="Email"][nbinput]')
+  //combine different selectors
+  page.locator('input[placeholder="Email"][nbinput]');
 
-    //by XPath (NOT RECOMMENDED)
-    page.locator('//*[@id="inputEmail1"]')
+  //by XPath (NOT RECOMMENDED)
+  page.locator('//*[@id="inputEmail1"]');
 
-    //by partial text match
-    page.locator(':text("Using")')
+  //by partial text match
+  page.locator(':text("Using")');
 
-    //by exact text match
-    page.locator(':text-is("Using the Grid")')
+  //by exact text match
+  page.locator(':text-is("Using the Grid")');
 });
 
-test('User facing Locators', async({page}) => {
-    await page.getByRole('textbox', {name: "Email"}).first().click();
-    await page.getByRole('button', {name: "Sign in"}).first().click();
-
-    await page.getByLabel('Email').first().click();
-    await page.getByPlaceholder('Jane Doe').click();
-    await page.getByText('Using the Grid').click();
-     await page.getByTestId('SignIn').click();
-      await page.getByTestId('SignIn').click();
-    await page.getByTitle('IoT Dashboard').click();
-   
+test("User facing Locators", async ({ page }) => {
+  await page.getByRole("textbox", { name: "Email" }).first().click();
+  await page.getByRole("button", { name: "Sign in" }).first().click();
+  await page.getByLabel("Email").first().click();
+  await page.getByPlaceholder("Jane Doe").click();
+  await page.getByText("Using the Grid").click();
+  await page.getByTestId("SignIn").click();
+  await page.getByTestId("SignIn").click();
+  await page.getByTitle("IoT Dashboard").click();
 });

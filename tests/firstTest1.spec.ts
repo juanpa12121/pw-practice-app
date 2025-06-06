7f066866b1d74c1845c41e13c3f7f711dcf1cb07
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2025-06-06 14:50:48 -0500

    29. Extracting Values

--- tests/firstTest1.spec.ts
+++ tests/firstTest1.spec.ts
@@ -87,3 +87,25 @@ test('Reusing locators', async ({ page }) => {
   await expect(emailField).toHaveValue("test@test.com");
   await expect(passwordField).toHaveValue("test123");
 });
+
+test('Extracting values', async ({ page }) => {
+  //Single test value
+  const basicForm = page.locator('nb-card').filter({hasText: "Basic Form"});
+  const buttonText = await basicForm.locator('button').textContent();
+  expect(buttonText).toBe("Submit");
+
+  //All text values
+  const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents();
+  expect(allRadioButtonsLabels).toEqual(["Option 1", "Option 2", "Disabled Option"]);
+  expect(allRadioButtonsLabels).toContain("Option 1");
+
+  //Input value
+  const emailField = basicForm.getByRole('textbox', {name: 'Email'});
+  await emailField.fill("test@test.com");
+  const emailValue = await emailField.inputValue();
+  expect(emailValue).toEqual("test@test.com");
+
+  const placeholderValue = await emailField.getAttribute('placeholder');
+  expect(placeholderValue).toEqual("Email");
+
+})
